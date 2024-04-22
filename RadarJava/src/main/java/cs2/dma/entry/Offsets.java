9bd67d8105ce1016e2c489a8d9ece6adf7067ed2
xCorteX <37551164+xCorteX@users.noreply.github.com>
2024-04-22 03:42:04 +0200

    Update Offsets.java
    uupdate for new CS2 Dumper

--- RadarJava/src/main/java/cs2/dma/entry/Offsets.java
+++ RadarJava/src/main/java/cs2/dma/entry/Offsets.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 public class Offsets {
     public  static long dwEntityList;
     public  static long dwLocalPlayerPawn;
-    public  static long mapNameVal=0x1CC200;
+    public  static long dwGameTypes_mapName;
 
     public  static long m_iTeamNum;
     public  static long m_hPlayerPawn;
@@ -24,16 +24,16 @@ public class Offsets {
         String client = new String(Files.readAllBytes(Paths.get("client.dll.json")));
         JSONObject offsetsJsonObj=JSONObject.parseObject(offsets);
         JSONObject clientJsonObj=JSONObject.parseObject(client);
-        dwEntityList=offsetsJsonObj.getJSONObject("client_dll").getJSONObject("data").getJSONObject("dwEntityList").getLong("value");
-        dwLocalPlayerPawn=offsetsJsonObj.getJSONObject("client_dll").getJSONObject("data").getJSONObject("dwLocalPlayerPawn").getLong("value");
-        m_iTeamNum=clientJsonObj.getJSONObject("C_BaseEntity").getJSONObject("data").getJSONObject("m_iTeamNum").getLong("value");
-        m_hPlayerPawn=clientJsonObj.getJSONObject("CCSPlayerController").getJSONObject("data").getJSONObject("m_hPlayerPawn").getLong("value");
-        m_angEyeAngles=clientJsonObj.getJSONObject("C_CSPlayerPawnBase").getJSONObject("data").getJSONObject("m_angEyeAngles").getLong("value");
-        m_iHealth=clientJsonObj.getJSONObject("C_BaseEntity").getJSONObject("data").getJSONObject("m_iHealth").getLong("value");
-        m_iPawnArmor=clientJsonObj.getJSONObject("CCSPlayerController").getJSONObject("data").getJSONObject("m_iPawnArmor").getLong("value");
-        m_lifeState=clientJsonObj.getJSONObject("C_BaseEntity").getJSONObject("data").getJSONObject("m_lifeState").getLong("value");
-        m_vOldOrigin=clientJsonObj.getJSONObject("C_BasePlayerPawn").getJSONObject("data").getJSONObject("m_vOldOrigin").getLong("value");
-
+        dwEntityList=           offsetsJsonObj.getJSONObject("client.dll")      .getLongValue("dwEntityList");
+        dwLocalPlayerPawn=      offsetsJsonObj.getJSONObject("client.dll")      .getLongValue("dwLocalPlayerPawn");
+        dwGameTypes_mapName=    offsetsJsonObj.getJSONObject("matchmaking.dll") .getLongValue("dwGameTypes_mapName");
+        m_iTeamNum=             clientJsonObj.getJSONObject("client.dll")       .getJSONObject("classes").getJSONObject("C_BaseEntity")        .getJSONObject("fields").getLongValue("m_iTeamNum");
+        m_hPlayerPawn=          clientJsonObj.getJSONObject("client.dll")       .getJSONObject("classes").getJSONObject("CCSPlayerController") .getJSONObject("fields").getLongValue("m_hPlayerPawn");
+        m_angEyeAngles=         clientJsonObj.getJSONObject("client.dll")       .getJSONObject("classes").getJSONObject("C_CSPlayerPawnBase")  .getJSONObject("fields").getLongValue("m_angEyeAngles");
+        m_iHealth=              clientJsonObj.getJSONObject("client.dll")       .getJSONObject("classes").getJSONObject("C_BaseEntity")        .getJSONObject("fields").getLongValue("m_iHealth");
+        m_iPawnArmor=           clientJsonObj.getJSONObject("client.dll")       .getJSONObject("classes").getJSONObject("CCSPlayerController") .getJSONObject("fields").getLongValue("m_iPawnArmor");
+        m_lifeState=            clientJsonObj.getJSONObject("client.dll")       .getJSONObject("classes").getJSONObject("C_BaseEntity")        .getJSONObject("fields").getLongValue("m_lifeState");
+        m_vOldOrigin=           clientJsonObj.getJSONObject("client.dll")       .getJSONObject("classes").getJSONObject("C_BasePlayerPawn")    .getJSONObject("fields").getLongValue("m_vOldOrigin");
     }
 
 }
